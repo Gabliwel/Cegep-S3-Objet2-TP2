3286add75153b0c855521e763ecd3a225d481b50
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2021-11-17 10:06:32 -0500

    initial commit

---
+++ src/main/java/outfitting/model/entity/Outfitting.java
@@ -0,0 +1,20 @@
+package outfitting.model.entity;
+
+public class Outfitting {
+
+	public static final int ID_ADDED_VALUE = 1;
+	
+	public static int lastId = 0;
+	
+	private int id;
+	private String name;
+	private String region;
+	private String phoneNumber;
+	private String email;
+	/*
+	public Outfitting(String name, String ) {
+		lastId += ID_ADDED_VALUE;
+		this.id = lastId;
+		this.name 
+	}*/
+}

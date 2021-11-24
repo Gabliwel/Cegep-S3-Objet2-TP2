de63ca9f4615515652c812bc548bf139e4bfc3a8
charles bertrand <1934088@csfoy.ca>
2021-11-24 09:44:14 -0500

    paufinage 2 plus de test

--- src/test/java/outfitting/controller/CottageListControllerTest.java
+++ src/test/java/outfitting/controller/CottageListControllerTest.java
@@ -4,12 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import outfitting.dto.CottageDTOForList;
+import outfitting.model.CottageMemoryRepositoryMockWithAdd;
 import outfitting.model.CottageRepositoryMock;
 import outfitting.model.entity.Cottage;
+import outfitting.model.entity.CottageMock;
 import outfitting.view.ViewMock;
 
 public class CottageListControllerTest {
@@ -26,22 +29,36 @@ public class CottageListControllerTest {
 		assertTrue(view.displayMethodHasBeenCalled);
 	}
 	
+//	@Test
+//	public void when_requestCottage_then_shouldAskCottageListToRepo() {
+//		ControllerOrchestratorMock orchestrator = new ControllerOrchestratorMock();
+//		ViewMock view = new ViewMock();
+//		CottageRepositoryMock repo = new CottageRepositoryMock();
+//		CottageListController controller = new CottageListController(orchestrator, view, repo);
+//		CottageDTOForList cottageDTO = new CottageDTOForList(1, "Bob", 3, 4, 5); //need this lign or else test will fail. cannot call .stream() in controller on a empty collection
+//		
+//		
+//		Collection<CottageDTOForList> cottages = repo;
+//		cottages.add(cottageDTO);
+//		
+//		controller.getCottageList();
+//		
+//		//assertNull(cottages);
+//		assertTrue(repo.getListBeenCalled);
+//	}
+	
 	@Test
-	public void when_requestCottage_then_shouldAskCottageListToRepo() {
+	public void WHEN_getListIsCalled_THEN_assuredThatGetListHasBeenCalled() 
+	{
 		ControllerOrchestratorMock orchestrator = new ControllerOrchestratorMock();
 		ViewMock view = new ViewMock();
-		CottageRepositoryMock repo = new CottageRepositoryMock();
+		CottageMemoryRepositoryMockWithAdd repo = new CottageMemoryRepositoryMockWithAdd();
 		CottageListController controller = new CottageListController(orchestrator, view, repo);
-		//CottageDTOForList cottageDTO = new CottageDTOForList(1, "Bob", 3, 4, 5); //need this lign or else test will fail. cannot call .stream() in controller on a empty collection
+		CottageMock cottage = new CottageMock("bob", 1, 2, 3);
 		
+		repo.add(cottage);
+		Collection<CottageDTOForList> cottageCollection = controller.getCottageList();
 		
-		
-		Collection<CottageDTOForList> cottages = controller.getCottageList();; 
-		//cottages.add(cottageDTO);
-		
-		
-		
-		//assertNull(cottages);
 		assertTrue(repo.getListBeenCalled);
 	}
 }
---
+++ src/test/java/outfitting/model/CottageMemoryRepositoryMockWithAdd.java
@@ -0,0 +1,47 @@
+package outfitting.model;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import outfitting.model.entity.Cottage;
+
+public class CottageMemoryRepositoryMockWithAdd implements CottageRepository {
+	
+	public boolean addHasBeenCalled = false;
+	public boolean getListBeenCalled = false;
+	public boolean removeHasBeenCalled = false;
+	
+	private Map<Integer, Cottage> cottages;
+	
+	public CottageMemoryRepositoryMockWithAdd()
+	{
+		cottages = new HashMap<>();
+	}
+	
+	@Override
+	public void add(Cottage cottage) {
+		cottages.put(cottage.getId(), cottage);
+	}
+
+	@Override
+	public Collection<Cottage> getList() {
+		getListBeenCalled = true;
+		return cottages.values();
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public Cottage searchById(int id) {
+		return null;
+	}
+
+	@Override
+	public void remove(int id, Cottage cottage) {
+		removeHasBeenCalled = true;
+	}
+}
